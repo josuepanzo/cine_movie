e127354133e279f7dc349664ba32419a5fc22ab4
josuepanzo <josuepanzo91@gmail.com>
2023-01-11 14:18:45 -0300

    Update my BestMovieOfYear adding a dynamic value

--- lib/common/best_movie_of_year.dart
+++ lib/common/best_movie_of_year.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../controller/controller_movie.dart';
+
 class BestMovieOfYear extends StatelessWidget {
-  const BestMovieOfYear({Key? key}) : super(key: key);
+  final int index;
+  // ignore: use_key_in_widget_constructors
+  const BestMovieOfYear(this.index);
   @override
   Widget build(BuildContext context) {
+    MovieController crtlMovie = MovieController();
     return Column(
       children: [
         Card(
@@ -13,13 +18,14 @@ class BestMovieOfYear extends StatelessWidget {
               Container(
                 width: 130,
                 height: 150,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
                     topLeft: Radius.circular(7),
                     bottomLeft: Radius.circular(7),
                   ),
                   image: DecorationImage(
-                    image: AssetImage("assets/image/donkey.png"),
+                    image:
+                        AssetImage(crtlMovie.bestMovieOfYearList[index].image),
                     fit: BoxFit.cover,
                   ),
                 ),
@@ -31,16 +37,18 @@ class BestMovieOfYear extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Donkey",
-                        style: TextStyle(
+                    Text(crtlMovie.bestMovieOfYearList[index].title,
+                        style: const TextStyle(
                           fontSize: 17,
                           fontWeight: FontWeight.bold,
                         )),
                     const SizedBox(height: 10),
-                    const SizedBox(
-                        width: 210,
-                        child: Text(
-                            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt laborum quos ratione excepturi ea facilis odio.")),
+                    SizedBox(
+                      width: 210,
+                      child: Text(
+                        crtlMovie.bestMovieOfYearList[index].description,
+                      ),
+                    ),
                   ],
                 ),
               ),
