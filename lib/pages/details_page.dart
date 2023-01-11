0e5748f8a75a59be6602f6690ed1804c2497ada5
josuepanzo <josuepanzo91@gmail.com>
2023-01-11 14:22:55 -0300

    Update a listview to listview.builder, and adding dynamic value

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../model/movie.dart';
 import '../widgets/custom_icon_card.dart';
 
+// ignore: must_be_immutable
 class DetailsPage extends StatelessWidget {
-  const DetailsPage({Key? key}) : super(key: key);
+  Movie movie;
+  DetailsPage({Key? key, required this.movie}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,101 +16,107 @@ class DetailsPage extends StatelessWidget {
         iconTheme: const IconThemeData(color: Colors.black),
         backgroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 20.0, bottom: 20),
-              child: Card(
-                elevation: 10,
-                child: Container(
-                  width: 260,
-                  height: 300,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: const DecorationImage(
-                      image: AssetImage("assets/image/donkey.png"),
-                      fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20.0, bottom: 20),
+                child: Card(
+                  elevation: 10,
+                  child: Container(
+                    width: 260,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage(movie.image),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-          const Text(
-            "Spider-Man: Into the Spider-Verse",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(
+                movie.title,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                  letterSpacing: 1,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
-            textAlign: TextAlign.center,
-          ),
-          Table(
-            children: [
-              const TableRow(
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 13.0),
+              child: Table(
                 children: [
-                  CustomIconCard(
-                    iconData: Icons.watch_later_outlined,
-                    label: "117 min",
-                  ),
-                  CustomIconCard(
-                    iconData: Icons.watch_later_outlined,
-                    label: "117 min",
-                  ),
-                  CustomIconCard(
-                    iconData: Icons.watch_later_outlined,
-                    label: "117 min",
+                  const TableRow(
+                    children: [
+                      CustomIconCard(
+                        iconData: Icons.watch_later_outlined,
+                        label: "117 min",
+                      ),
+                      CustomIconCard(
+                        iconData: Icons.calendar_today,
+                        label: "2023",
+                      ),
+                      CustomIconCard(
+                        iconData: Icons.star_border,
+                        label: "8.4/10",
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-          const Text(
-            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt laborum quos ratione excepturi ea facilis odio.",
-            style: TextStyle(height: 1, fontSize: 16),
-            textAlign: TextAlign.center,
-          ),
-        ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(
+                movie.description,
+                style: const TextStyle(height: 1.2, letterSpacing: .8, fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 15),
+          ],
+        ),
       ),
       bottomNavigationBar: Row(
         children: [
           Expanded(
             child: Container(
               color: Theme.of(context).primaryColor,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: const [
-                      Icon(Icons.play_circle_outline_sharp,
-                          color: Colors.white),
-                      Text('Watch Trailer',
-                          style: TextStyle(color: Colors.white)),
-                    ],
+              padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(Icons.check_circle_outline, color: Colors.white),
+                  Text(
+                    'Watch Trailer',
+                    style: TextStyle(color: Colors.white),
                   ),
-                ),
+                ],
               ),
             ),
           ),
           Expanded(
             child: Container(
               color: Colors.yellowAccent,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: const [
-                      Icon(Icons.play_circle_outline_sharp,
-                          color: Colors.black),
-                      Text('Buy Now', style: TextStyle(color: Colors.black)),
-                    ],
+              padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(Icons.monetization_on, color: Colors.black),
+                  Text(
+                    'Buy Now',
+                    style: TextStyle(color: Colors.black),
                   ),
-                ),
+                ],
               ),
             ),
           ),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,79 @@
 import 'package:flutter/material.dart';
+import '../common/top_rated_movie.dart';
+import '../widgets/custom_text_row.dart';
+import '../common/best_movie_of_year.dart';
+import '../common/best_movie_of_weak.dart';
+import '../controller/controller_movie.dart';
+import 'details_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    MovieController crtlMovie = MovieController();
     return Scaffold(
       appBar: AppBar(
         title: const Text("CineMovie", style: TextStyle(color: Colors.white)),
         centerTitle: true,
+        elevation: 10,
       ),
       body: SingleChildScrollView(
-        child: Column(children: const [],),
+        child: Column(
+          children: [
+            CustomTextRow(label: "Movie of the week", onPressed: () {}),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 260,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: crtlMovie.bestMovieOfWeakList.length,
+                itemBuilder: (context, index) => InkWell(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailsPage(movie: crtlMovie.bestMovieOfWeakList[index]),
+                        )),
+                    child: BestMovieOfWeak(index),
+                  ),
+            ),
+            ),
+            CustomTextRow(label: "Best Movie of 2023", onPressed: () {}),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 500,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: crtlMovie.bestMovieOfYearList.length,
+                itemBuilder: (context, index) => InkWell(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailsPage(movie: crtlMovie.bestMovieOfYearList[index]),
+                        )),
+                    child: BestMovieOfYear(index),
+                  ),
+              ),
+            ),
+            CustomTextRow(label: "Top Rated Movie", onPressed: () {}),
+            Container(
+              height: 260,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: crtlMovie.topRatedMovieList.length,
+                itemBuilder: (context, index) => InkWell(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailsPage(movie: crtlMovie.topRatedMovieList[index],),
+                        ),),
+                    child: TopRatedMovie(index),
+                  ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
