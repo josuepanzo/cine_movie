322bec879a61197d63ae212fbd3981fa7228ce4e
josuepanzo <josuepanzo91@gmail.com>
2023-01-11 14:13:42 -0300

    Adding a Model movie

---
+++ lib/model/movie.dart
@@ -0,0 +1,13 @@
+class Movie {
+  final int id;
+  final String title;
+  final String description;
+  final String image;
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+}
