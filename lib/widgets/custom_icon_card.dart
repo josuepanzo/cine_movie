a04b1a016f18acad293ab69d5af0e59013e436ff
josuepanzo <josuepanzo91@gmail.com>
2023-01-11 10:07:39 -0300

    Adding a CustomIconCard on DetailsPage

---
+++ lib/widgets/custom_icon_card.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomIconCard extends StatelessWidget {
+  final IconData iconData;
+  final String label;
+  const CustomIconCard({super.key, required this.iconData, required this.label,});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 28, bottom: 28,),
+      child: Card(
+        elevation: 8,
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            children: [
+              Icon(iconData, color: Colors.indigo, size: 50),
+              const SizedBox(height: 18),
+              Text(
+                label,
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
